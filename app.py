657020028ce982ab87616ddd23bc78ce914e8528
michaelskyba <michaelskyba1411@gmail.com>
2020-08-07 11:56:52 -0400

    Used Jinja to make index.html completely finished
    Now, it will only show "register" and "sign in" if the user isn't logged in

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ app = Flask(__name__)
 # This saves a bunch of time, so I don't have to reload it manually to test things
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 # Configure flask so that it stores session files in a temporary filesystem instead of using cookies
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
@@ -23,10 +31,24 @@ Session(app)
 # Configure the database
 db = SQL("sqlite:///mt.db")
 
+
 # Homepage
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Check if user is logged in
+    if not "username" in session.keys():
+        user = ""
+    else:
+        user = session["username"]
+
+    # Serves the index.html page
+    return render_template("index.html", user=user)
+
+# Sign in page
+@app.route("/signin")
+def signin():
+    session["username"] = "it works!"
+    return redirect("/")
 
 
 
