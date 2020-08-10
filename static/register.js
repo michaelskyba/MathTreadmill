c00e4c30da8a0401c26f47287f4d2453b6194280
michaelskyba <michaelskyba1411@gmail.com>
2020-08-10 12:49:47 -0400

    Fixed bug where you could register with a username longer than 15 chars

--- static/register.js
+++ static/register.js
@@ -4,7 +4,9 @@ document.getElementById("submit_button").onclick = function()
 	let status_p = document.getElementById("status");
 
 	if (document.getElementById("username").value == "") status_p.innerHTML = "'username' input must have a value";
-	else if (document.getElementById("password").value = "") status_p.innerHTML = "'password' input must have a value";
-	else if (document.getElementById("username").length > 15) status_p.innerHTML = "'username' input may not be longer than 15 characters";
+	else if (document.getElementById("password").value == "") status_p.innerHTML = "'password' input must have a value";
+	else if (document.getElementById("confirmation").value == "") status_p.innerHTML = "'password (again)' input must have a value";
+	else if (document.getElementById("password").value != document.getElementById("confirmation").value) status_p.innerHTML = "passwords must match";
+	else if (document.getElementById("username").value.length > 15) status_p.innerHTML = "'username' input may not be longer than 15 characters";
 	else document.getElementById("content").submit();
 }
