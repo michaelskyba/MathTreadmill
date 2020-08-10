00c63e16b384f4192841cd99e57b6cbba339423c
michaelskyba <michaelskyba1411@gmail.com>
2020-08-10 12:54:57 -0400

    Fixed /auto making you divide by zero

--- static/auto.js
+++ static/auto.js
@@ -587,5 +587,10 @@ function generate_question(skill)
 
 function RNG(min, max)
 {
-	return (Math.floor(Math.random()*(max - min + 1)) + min);
+	let r = 0;
+	while (r == 0)
+	{
+		r = (Math.floor(Math.random()*(max - min + 1)) + min);
+	}
+	return r;
 }
