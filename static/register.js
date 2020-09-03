10eec371db3b4cd0b4f72fb0201744c2da215e39
michaelskyba <michaelskyba1411@gmail.com>
2020-09-03 17:27:35 -0400

    Made it so pressing enter on the sign in and register pages submits the form without having to click 'Submit'

--- static/register.js
+++ static/register.js
@@ -1,12 +1,43 @@
 
-document.getElementById("submit_button").onclick = function()
+function submit_form()
 {
 	let status_p = document.getElementById("status");
 
-	if (document.getElementById("username").value == "") status_p.innerHTML = "'username' input must have a value";
-	else if (document.getElementById("password").value == "") status_p.innerHTML = "'password' input must have a value";
-	else if (document.getElementById("confirmation").value == "") status_p.innerHTML = "'password (again)' input must have a value";
-	else if (document.getElementById("password").value != document.getElementById("confirmation").value) status_p.innerHTML = "passwords must match";
-	else if (document.getElementById("username").value.length > 15) status_p.innerHTML = "'username' input may not be longer than 15 characters";
+	if (document.getElementById("username").value == "")
+	{
+		status_p.innerHTML = "'username' input must have a value";
+	}
+	else if (document.getElementById("password").value == "")
+	{
+		status_p.innerHTML = "'password' input must have a value";
+	}
+	else if (document.getElementById("confirmation").value == "")
+	{
+		status_p.innerHTML = "'password (again)' input must have a value";
+	}
+	else if (document.getElementById("password").value != document.getElementById("confirmation").value)
+	{
+		status_p.innerHTML = "passwords must match";
+	} 
+	else if (document.getElementById("username").value.length > 15)
+	{
+		status_p.innerHTML = "'username' input may not be longer than 15 characters";
+	}
 	else document.getElementById("content").submit();
 }
+
+
+document.getElementById("submit_button").onclick = function()
+{
+	submit_form();
+}
+
+
+document.onkeydown = function(e)
+{
+	e = e || window.event;
+
+	// When user clicks Enter, submit the form (UX)
+	if (e.keyCode == 13) submit_form();
+}
+
--- static/signin.js
+++ static/signin.js
@@ -1,8 +1,24 @@
 
-document.getElementById("submit_button").onclick = function()
+function submit_form()
 {
 	let status_p = document.getElementById("status");
 	if (document.getElementById("username").value == "") status_p.innerHTML = "'username' input must have a value";
 	else if (document.getElementById("password").value == "") status_p.innerHTML = "'password' input must have a value";
 	else document.getElementById("content").submit();
 }
+
+
+document.getElementById("submit_button").onclick = function()
+{
+	submit_form();
+}
+
+
+document.onkeydown = function(e)
+{
+	e = e || window.event;
+
+	// When user clicks Enter, submit the form (UX)
+	if (e.keyCode == 13) submit_form();
+}
+
